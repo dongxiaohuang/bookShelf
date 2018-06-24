6fa861545d4f4853b8eb3202b47b9f05ca6bff26
dongxiaohuang <dongxiao.huang17@imperial.ac.uk>
2018-06-24 11:43:02 +0100

    initial book shelf project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LibraryComponent } from './library/library.component';
+import { PagerComponent } from './pager/pager.component';
+import { SearchComponent } from './search/search.component';
+import { HeaderComponent } from './header/header.component';
+import { BookListComponent } from './book-list/book-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LibraryComponent,
+    PagerComponent,
+    SearchComponent,
+    HeaderComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule
